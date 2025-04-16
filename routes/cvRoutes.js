9d4ca94a711cce5fcc15e00e343551079b85f677
Rajih kharissuha <rajhk704@gmail.com>
2025-04-16 21:24:12 +0700

    Change Upload multer

--- routes/cvRoutes.js
+++ routes/cvRoutes.js
@@ -6,6 +6,6 @@ const { handleUploadCVs } = require("../controllers/cvController");
 const storage = multer.memoryStorage(); // tidak simpan di disk
 const upload = multer({ storage });
 
-router.post("/upload", upload.array("cvs", 10), handleUploadCVs);
+router.post("/upload", upload.array("cvs", 100), handleUploadCVs);
 
 module.exports = router;
