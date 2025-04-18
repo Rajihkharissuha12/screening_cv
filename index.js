774d1056e48a5d69ddb092cd421c043c7e3da5ab
Rajih kharissuha <rajhk704@gmail.com>
2025-04-18 20:56:25 +0700

    Add Cors

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors"); // 1. Import cors
 const ScreeningRoutes = require("./routes/cvRoutes");
 const UploadRoutes = require("./routes/uploadRoutes");
+
+// 2. Configure CORS to allow only your frontend origin
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace 3001 with your actual Next.js frontend port if different
+};
+app.use(cors(corsOptions)); // Use the configured options
 
 app.use(express.json());
 app.use("/api/cv", ScreeningRoutes);
